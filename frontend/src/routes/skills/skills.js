efe07a246cb8d8876285fa93309d49a30403245f
RohaanMooken <rohaanmooken@gmail.com>
2023-06-18 17:14:43 +0200

    Axios api retrieves skills from backend

--- frontend/src/routes/skills/skills.js
+++ frontend/src/routes/skills/skills.js
@@ -1,27 +1,55 @@
 import HeaderContainer from "../../components/headerContainer/headerContainer";
 import "./skills.css";
 import data from "../../assets/skills.json";
+import React from 'react';
+import axios from 'axios';
 
-function Card() {
-  return (
-    <div className="card">
-      Text
-    </div>
-  );
-}
 
-function CardGenerator() {
-  return (
-    <div>
-      <Card />
-    </div>
-  );
+class AxiosAPI extends React.Component {
+
+  state = { details: [], }
+
+  componentDidMount() {
+
+    let data;
+    axios.get('http://localhost:8000')
+      .then(res => {
+        data = res.data
+        this.setState({
+          details: data
+        });
+      })
+      .catch(err => { console.log("[ERROR] ", err)});
+  }
+
+  render() {
+    console.log(this.state)
+    return (
+      <div className="cardContainer">
+        {this.state.details.map((output, id) => (
+          <div>
+            <div key={id}>
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">{output.skill_name}</h5>
+                  <p className="card-text">{output.skill_level}</p>
+                </div>
+                <div className="card-footer">
+                  <small className="text-muted">{"Last updated: " + output.last_updated}</small>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    )
+  }
 }
 
 function CardContainer() {
   return (
     <div>
-      <CardGenerator />
+      <AxiosAPI />
     </div>
   );
 }
